d312acd2cec145c5c30c416067f8bc3f7ad3f6e2
Bryan <h.bryan68@yahoo.ca>
2025-03-22 23:18:53 -0400

    create media

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,7 @@ export const NAV_LINKS = [
     { href: '/', key: 'home', label: 'Home' },
     { href: '/about', key: 'about_us', label: 'About Us' },
     { href: '/', key: 'performaces', label: 'Performances' },
-    { href: '/', key: 'media', label: 'Media' },
+    { href: '/media', key: 'media', label: 'Media' },
     { href: '/', key: 'booking ', label: 'Booking ' },
   ];
 
