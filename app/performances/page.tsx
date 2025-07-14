3e9095c3870423ac55748f18de7c6c6246513222
Bryan <h.bryan68@yahoo.ca>
2025-07-14 13:50:55 -0400

    Performances Created

---
+++ app/performances/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-container padding-container flex flex-col gap-16 my-10">
+      Coming soon
+    </div>
+  );
+};
+
+export default page;
