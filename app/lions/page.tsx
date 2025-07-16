f5ac7e00014e10720be48e63b366eb1082d20118
Bryan <h.bryan68@yahoo.ca>
2025-07-16 10:47:22 -0400

    remade divs for Meet Lions page. images in center and resize according to window. 1 image per row. line added between Main lions and LED Lions.

--- app/lions/page.tsx
+++ app/lions/page.tsx
@@ -7,37 +7,46 @@ const page = () => {
     <div className="max-container padding-container flex flex-col gap-16 my-10">
       <h1 className="bold-32 text-gold-100">Meet the Lions</h1>
       <section className="mb-10">
-        <div>
-          <h2 className="bold-20">Main Lions</h2>
-          <ul>
-            {LIONS.map((lion) => (
-              <div>
-                <Image
-                  src={lion.image}
-                  alt="lion picture"
-                  width={100}
-                  height={100}
-                />
-                <span>{lion.name}. </span>
-                <span>{lion.colors}</span>
-              </div>
-            ))}
-          </ul>
-          <h2 className="bold-20">LED Lions</h2>
-          <ul>
-            {LEDLIONS.map((lion) => (
-              <div>
-                <Image
-                  src={lion.image}
-                  alt="lion picture"
-                  width={100}
-                  height={100}
-                />
-                <span>{lion.name}. </span>
-                <span>{lion.colors}</span>
-              </div>
-            ))}
-          </ul>
+        <div className="flex flex-col gap-16 my-10">
+          <div className="flexCenter flex-col">
+            <h2 className="bold-32">Main Lions</h2>
+            <div className="flex flex-col flex-initial justify-center">
+              <ul className="flex justify-evenly flex-col flex-wrap lg:flex-row gap-16 my-10">
+                {LIONS.map((lion) => (
+                  <div>
+                    <Image
+                      src={lion.image}
+                      alt="lion picture"
+                      width={700}
+                      height={400}
+                    />
+                    <span>{lion.name}. </span>
+                    <span>{lion.colors}</span>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <hr className=" shadow " />
+          <div className="flexCenter flex-col">
+            <h2 className="bold-32">LED Lions</h2>
+            <div className="flex flex-col flex-initial justify-center">
+              <ul className="flex justify-evenly flex-col flex-wrap lg:flex-row gap-16 my-10">
+                {LEDLIONS.map((lion) => (
+                  <div>
+                    <Image
+                      src={lion.image}
+                      alt="lion picture"
+                      width={700}
+                      height={400}
+                    />
+                    <span>{lion.name}. </span>
+                    <span>{lion.colors}</span>
+                  </div>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
     </div>
