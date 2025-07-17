748ff24f3201dd6e3ec09345845f159ed19246fa
Bryan <h.bryan68@yahoo.ca>
2025-07-17 17:46:38 -0400

    framer motion home page first section

--- components/Group.tsx
+++ components/Group.tsx
@@ -1,46 +1,75 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { motion, AnimatePresence, delay } from "framer-motion";
 
 const Group = () => {
   return (
     <section>
       <div className=" max-container padding-container flexCenter flex-col gap-10 py-5 pb-24 md:gap-10 lg:py-10">
-        {/* Logo */}
-        <Image
-          width={200}
-          height={200}
-          src="/ymt-logo.png"
-          alt="YMT Logo"
-        ></Image>
+        <AnimatePresence>
+          <motion.div
+            whileInView={{
+              y: 0,
+              opacity: 1,
+              transition: { delay: 0.5 },
+            }}
+            transition={{ duration: 1 }}
+            initial={{ y: 20, opacity: 0 }}
+          >
+            {/* Logo */}
+            <Image
+              width={200}
+              height={200}
+              src="/ymt-logo.png"
+              alt="YMT Logo"
+            ></Image>
+          </motion.div>
+          {/* Intro Message */}
+          <motion.div
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.75 }}
+            initial={{ y: 20, opacity: 0 }}
+            className="max-w-3xl"
+          >
+            <h2 className="pb-10 bold-32 text-center text-gold-100">
+              YMT Toronto Lion Dance
+            </h2>
+            <p className="text-justify regular-18">
+              YMT Toronto Lion Dance – Yi Mo Tong is a team with a rich legacy
+              in the art of lion dance, building on over 25 years of experience.
+              For the past decade, we established a blend of athleticism and
+              artistry, aimed at spreading cultural awareness and inspiring new
+              generations to embrace this ancient tradition. We celebrated a
+              major milestone by securing 3rd place at the prestigious 2024
+              Canada National Competition. As we look to the future, our sights
+              are set on competing internationally and to elevate both the sport
+              and the cultural significant of Lion Dance in Canada.
+            </p>
+          </motion.div>
 
-        {/* Intro Message */}
-        <div className="max-w-3xl">
-          <h2 className="pb-10 bold-32 text-center text-gold-100">
-            YMT Toronto Lion Dance
-          </h2>
-          <p className="text-justify regular-18">
-            YMT Toronto Lion Dance – Yi Mo Tong is a team with a rich legacy in
-            the art of lion dance, building on over 25 years of experience. For
-            the past decade, we established a blend of athleticism and artistry,
-            aimed at spreading cultural awareness and inspiring new generations
-            to embrace this ancient tradition. We celebrated a major milestone
-            by securing 3rd place at the prestigious 2024 Canada National
-            Competition. As we look to the future, our sights are set on
-            competing internationally and to elevate both the sport and the
-            cultural significant of Lion Dance in Canada.
-          </p>
-        </div>
-
-        {/* Book Button */}
-        <div className="flexCenter mb-20 py-5">
-          <Link href="/booking">
-            <button className="px-16 py-2 regular-32 shadow-md bg-gold-100 text-white rounded-lg hover:bg-gold-120">
-              Book Now ↗
-            </button>
-          </Link>
-        </div>
-        {/* Group Photo */}
-        <img src="/group.jpg" alt="group photo"></img>
+          {/* Book Button */}
+          <motion.div
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 1 }}
+            initial={{ y: 20, opacity: 0 }}
+            className="flexCenter mb-20 py-5"
+          >
+            <Link href="/booking">
+              <button className="px-16 py-2 regular-32 shadow-md bg-gold-100 text-white rounded-lg hover:bg-gold-120">
+                Book Now ↗
+              </button>
+            </Link>
+          </motion.div>
+          {/* Group Photo */}
+          <motion.div
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 1.25 }}
+            initial={{ y: 20, opacity: 0 }}
+          >
+            <img src="/group.jpg" alt="group photo"></img>
+          </motion.div>
+        </AnimatePresence>
       </div>
     </section>
   );
