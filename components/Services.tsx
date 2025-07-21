e028d7d14a862a9c1fd41886f19cedda3b5f9a81
Bryan <h.bryan68@yahoo.ca>
2025-07-21 15:48:03 -0400

    added motion to the services cards

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,54 +1,84 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+
 const Services = () => {
   return (
     <section className=" max-container padding-container mb-10">
-      <div className="flexCenter flex-col gap-10 py-5 px-5">
-        <h1 className="bold-52 text-gold-100">Our Services</h1>
-        <div className="flex flex-col lg:flex-row">
-          <div className="flexCenter flex-col gap-10 py-5 px-5">
-            <h2 className="text-center regular-32 text-gold-100 ">Weddings</h2>
-            <div className="flexCenter flex-col gap-5">
-              <img
-                src="/wedding.jpeg"
-                alt="wedding"
-                className="h-96 w-96 object-cover"
-              ></img>
-              <p className="text-center p-5 md:regular-24 ">
-                Make your special day unforgettable!
-              </p>
-            </div>
+      <AnimatePresence>
+        <motion.div className="flexCenter flex-col gap-10 py-5 px-5">
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+            viewport={{ once: true, margin: "-100px" }}
+            className="bold-52 text-gold-100"
+          >
+            Our Services
+          </motion.h1>
+          {/* Title - Image Card - Caption */}
+          <div className="flex flex-col lg:flex-row">
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+              viewport={{ once: true, margin: "-100px" }}
+              className="flexCenter flex-col gap-10 py-5 px-5"
+            >
+              <h2 className="text-center regular-32 text-gold-100 ">
+                Weddings
+              </h2>
+              <div className="flexCenter flex-col gap-5">
+                <img
+                  src="/wedding.jpeg"
+                  alt="wedding"
+                  className="h-96 w-96 object-cover"
+                ></img>
+                <p className="text-center p-5 md:regular-24 ">
+                  Make your special day unforgettable!
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+              viewport={{ once: true, margin: "-100px" }}
+              className="flexCenter flex-col gap-10 py-5 px-5"
+            >
+              <h2 className="text-center regular-32 text-gold-100">
+                Special Celebrations
+              </h2>
+              <div className="flexCenter flex-col gap-5">
+                <img
+                  src="/celebrations.jpg"
+                  alt="special"
+                  className="h-96 w-96 object-cover"
+                ></img>
+                <p className="text-center p-5 md:regular-24">
+                  Add excitement to your party!
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+              viewport={{ once: true, margin: "-100px" }}
+              className="flexCenter flex-col gap-10 py-5 px-5"
+            >
+              <h2 className="text-center regular-32 text-gold-100">
+                Corporate Events
+              </h2>
+              <div className="flexCenter flex-col gap-5">
+                <img
+                  src="/corporate.jpeg"
+                  alt="corporate events"
+                  className="h-96 w-96 object-cover"
+                ></img>
+                <p className="text-center p-5 md:regular-24">
+                  Impress clients and boost morale!
+                </p>
+              </div>
+            </motion.div>
           </div>
-          <div className="flexCenter flex-col gap-10 py-5 px-5">
-            <h2 className="text-center regular-32 text-gold-100">
-              Special Celebrations
-            </h2>
-            <div className="flexCenter flex-col gap-5">
-              <img
-                src="/celebrations.jpg"
-                alt="special"
-                className="h-96 w-96 object-cover"
-              ></img>
-              <p className="text-center p-5 md:regular-24">
-                Add excitement to your party!
-              </p>
-            </div>
-          </div>
-          <div className="flexCenter flex-col gap-10 py-5 px-5">
-            <h2 className="text-center regular-32 text-gold-100">
-              Corporate Events
-            </h2>
-            <div className="flexCenter flex-col gap-5">
-              <img
-                src="/corporate.jpeg"
-                alt="corporate events"
-                className="h-96 w-96 object-cover"
-              ></img>
-              <p className="text-center p-5 md:regular-24">
-                Impress clients and boost morale!
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </section>
   );
 };
