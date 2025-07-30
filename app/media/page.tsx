1be430a3adeb4947fa431fa13f9b4dbecc557600
Bryan_w11 <h.bryan68@yahoo.ca>
2025-07-30 12:26:13 -0400

    documentary embeded

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -5,15 +5,20 @@ const page = () => {
   return (
     <div className="max-container padding-container flex flex-col gap-16 my-10">
       <h1 className="bold-32 text-gold-100">Media</h1>
-      <section className="mb-10">
-        <ul className="flexCenter flex-col gap-20">
-          {MEDIA_VIDEOS.map((video) => (
-            <video width={512} height={768} muted controls preload="auto">
-              <source src={video.src} type={video.type} />
-              Your browser does not support the video tag.
-            </video>
-          ))}
-        </ul>
+      <section className="flexCenter flex-col gap-10 mb-10">
+        <h2 className="bold-20">
+          Heart of The Lion - YMT Lion Dance Documentary, Toronto
+        </h2>
+        <div className="w-full max-w-[854px] max-h-[480px] mx-auto aspect-video">
+          <iframe
+            src="https://www.youtube.com/embed/KaubRCbi7Bc?si=VCsVydF_4hL6SFlm"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            className="w-full h-full"
+          />
+        </div>
       </section>
     </div>
   );
