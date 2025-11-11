f62db7b1a7fb8b88993e700f9b7d448c732ff30b
Bryan_w11 <h.bryan68@yahoo.ca>
2025-11-11 14:26:09 -0500

    added load in animations to meet the lions and media page

--- app/lions/page.tsx
+++ app/lions/page.tsx
@@ -1,55 +1,65 @@
+"use client";
 import React from "react";
 import { LIONS, LEDLIONS } from "@/constants";
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
+import { useFadeInRightDelay } from "@/constants/motionVariants";
 
 const page = () => {
   return (
-    <div className="max-container padding-container flex flex-col gap-16 my-10">
-      <h1 className="bold-32 text-gold-100">Meet the Lions</h1>
-      <section className="mb-10">
-        <div className="flex flex-col gap-16 my-10">
-          <div className="flexCenter flex-col">
-            <h2 className="bold-32">Main Lions</h2>
-            <div className="flex flex-col flex-initial justify-center">
-              <ul className="flex justify-evenly flex-col flex-wrap lg:grid-cols-1 gap-16 my-10">
-                {LIONS.map((lion) => (
-                  <div>
-                    <Image
-                      src={lion.image}
-                      alt="lion picture"
-                      width={400}
-                      height={400}
-                    />
-                    <span>{lion.name}. </span>
-                    <span>{lion.colors}</span>
-                  </div>
-                ))}
-              </ul>
+    <AnimatePresence>
+      <motion.div
+        variants={useFadeInRightDelay(0.1)}
+        initial="hidden"
+        animate="show"
+        className="max-container padding-container flex flex-col gap-16 my-10"
+      >
+        <h1 className="bold-32 text-gold-100">Meet the Lions</h1>
+        <section className="mb-10">
+          <div className="flex flex-col gap-16 my-10">
+            <div className="flexCenter flex-col">
+              <h2 className="bold-32">Main Lions</h2>
+              <div className="flex flex-col flex-initial justify-center">
+                <ul className="flex justify-evenly flex-col flex-wrap lg:grid-cols-1 gap-16 my-10">
+                  {LIONS.map((lion) => (
+                    <div>
+                      <Image
+                        src={lion.image}
+                        alt="lion picture"
+                        width={400}
+                        height={400}
+                      />
+                      <span>{lion.name}. </span>
+                      <span>{lion.colors}</span>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <hr className=" shadow " />
+            <div className="flexCenter flex-col">
+              <h2 className="bold-32">LED Lions</h2>
+              <div className="flex flex-col flex-initial justify-center">
+                <ul className="flex justify-evenly flex-col flex-wrap lg:grid-cols-1 gap-16 my-10">
+                  {LEDLIONS.map((lion) => (
+                    <div>
+                      <Image
+                        src={lion.image}
+                        alt="lion picture"
+                        width={400}
+                        height={400}
+                      />
+                      <span>{lion.name}. </span>
+                      <span>{lion.colors}</span>
+                    </div>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
-          <hr className=" shadow " />
-          <div className="flexCenter flex-col">
-            <h2 className="bold-32">LED Lions</h2>
-            <div className="flex flex-col flex-initial justify-center">
-              <ul className="flex justify-evenly flex-col flex-wrap lg:grid-cols-1 gap-16 my-10">
-                {LEDLIONS.map((lion) => (
-                  <div>
-                    <Image
-                      src={lion.image}
-                      alt="lion picture"
-                      width={400}
-                      height={400}
-                    />
-                    <span>{lion.name}. </span>
-                    <span>{lion.colors}</span>
-                  </div>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,21 +1,31 @@
+"use client";
 import YTVideo from "@/components/YTVideo";
 import { MEDIA_VIDEOS } from "@/constants";
+import { useFadeInRightDelay } from "@/constants/motionVariants";
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="max-container padding-container flex flex-col gap-16 my-10">
-      <h1 className="bold-32 text-gold-100">Media</h1>
-      <section className="flexCenter flex-col gap-10 mb-10">
-        <div>
-          <ul className="max-container padding-container flex flex-col gap-16 my-10 ">
-            {MEDIA_VIDEOS.map((video) => (
-              <YTVideo title={video.title} src={video.src} />
-            ))}
-          </ul>
-        </div>
-      </section>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        variants={useFadeInRightDelay(0.1)}
+        initial="hidden"
+        animate="show"
+        className="max-container padding-container flex flex-col gap-16 my-10"
+      >
+        <h1 className="bold-32 text-gold-100">Media</h1>
+        <section className="flexCenter flex-col gap-10 mb-10">
+          <div>
+            <ul className="max-container padding-container flex flex-col gap-16 my-10 ">
+              {MEDIA_VIDEOS.map((video) => (
+                <YTVideo title={video.title} src={video.src} />
+              ))}
+            </ul>
+          </div>
+        </section>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
